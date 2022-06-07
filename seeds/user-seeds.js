634c91b79a412389e529ef20d486b5f142f83acf
Franklyn Tavarez <kurbby.450@gmail.com>
2022-06-06 21:50:52 -0700

    utils

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,42 +3,42 @@ const { User } = require('../models');
 const userData = [
     {
         username: "Tina_bina",
-        twitter: "Tinab",
+        instagram: "Tinab",
         github: "Tinab",
-        email: "Tina_b@gmail.com",
+        email: "Tina_bina@gmail.com",
         password: "p@ssword1"
     },
     {
         username: "john_a",
-        twitter: "johna",
+        instagram: "johna",
         github: "johna",
         email: "john_a@gmail.com",
         password: "p@ssword2"
     },
     {
         username: "tom_t",
-        twitter: "tom",
+        instagram: "tom",
         github: "tom",
         email: "tom_t@gmail.com",
         password: "p@ssword3"
     },
     {
         username: "bruce_j",
-        twitter: "brucejunior",
+        instagram: "brucejunior",
         github: "brucejunior",
         email: "bruce_j@gmail.com",
         password: "p@ssword4"
     },
     {
         username: "keny_t",
-        twitter: "kenytom3",
+        instagram: "kenytom3",
         github: "kenytom3",
         email: "keny_t@gmail.com",
         password: "p@ssword5"
     },
     {
         username: "xaiver",
-        twitter: "xaiver_w",
+        instagram: "xaiver_w",
         github: "xaiver",
         email: "xaiver_w@gmail.com",
         password: "p@ssword6"
