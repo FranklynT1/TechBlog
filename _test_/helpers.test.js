39ab46d758fa704b7d36549e72746aa0de6bf7df
Franklyn Tavarez <kurbby.450@gmail.com>
2022-06-06 19:38:17 -0700

    add test.js

---
+++ _test_/helpers.test.js
@@ -0,0 +1,29 @@
+const {format_date} = require('../utils/helpers');
+
+test('format_date() returns a date string', () => {
+    const date = new Date('');
+  
+    expect(format_date(date)).toBe('');
+  });
+
+const {format_plural} = require('../utils/helpers')
+
+test('format_plural() returns a pluralized word', () => {
+      const plural = format_plural('toocan', 2);
+      const single = format_plural('mastif', 1);
+      
+      expect(plural).toBe('toocan');
+      expect(single).toBe('mastif');
+});
+
+const {format_url} = require('../utils/helpers');
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.awsomeness.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('asomeness.com');
+    expect(url3).toBe('google.com');
+});
